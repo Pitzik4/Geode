4f8098973a725f467097a732bb7c835cd64fb584
misson20000 <collin@collinswebsite.com>
2012-03-24 15:59:58 -0700

    Not sure

--- src/misson20000/api/vorxel/Vorxel.java
+++ src/misson20000/api/vorxel/Vorxel.java
@@ -3,6 +3,7 @@ package misson20000.api.vorxel;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 import pitzik4.geode.Geode;
 
@@ -19,6 +20,11 @@ public class Vorxel {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	}
 	
 	public static void main(String[] args) {
@@ -36,5 +42,22 @@ public class Vorxel {
 			Display.destroy();
 			System.exit(0);
 		}
+		render();
+	}
+
+	private static void render() {
+		System.out.println("Render");
+		// TODO Auto-generated method stub
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); 
+		// set the color of the quad (R,G,B,A)
+		GL11.glColor3f(0.5f,0.5f,1.0f);
+		 
+		// draw quad
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2f(100,100);
+		GL11.glVertex2f(100+200,100);
+		GL11.glVertex2f(100+200,100+200);
+		GL11.glVertex2f(100,100+200);
+		GL11.glEnd();
 	}
 }
