ff90d7dbacb73402f89f7588904051883c915223
Bob McPants <git@maxisplayful.com>
2012-03-24 16:21:52 -0700

    Added a ticking system

--- src/misson20000/api/vorxel/Vorxel.java
+++ src/misson20000/api/vorxel/Vorxel.java
@@ -9,11 +9,10 @@ import pitzik4.geode.Geode;
 public class Vorxel {
 	private static Geode game;
 
-	public static void init(Settings set, Geode gamep) {
+	public static void init(VorxelSettings set, Geode gamep) {
 		game = gamep;
 		try {
 			Display.setDisplayMode(new DisplayMode(set.width,set.height));
-			new ThreadTick().start();
 			Display.create();
 		} catch (LWJGLException e) {
 			// TODO Auto-generated catch block
@@ -22,19 +21,16 @@ public class Vorxel {
 	}
 	
 	public static void main(String[] args) {
-		init(new Settings(), null);
+		init(new VorxelSettings(), null);
 	}
 
 	public static void tick() {
-		// TODO Auto-generated method stub
-		if(game != null) {
-			game.tick();
-		}
-		Display.update();
-		if(Display.isCloseRequested()) {
-			game.close();
-			Display.destroy();
-			System.exit(0);
+		if(Display.isCreated()) {
+			Display.update();
+			if(Display.isCloseRequested()) {
+				game.close();
+				Display.destroy();
+			}
 		}
 	}
 }
