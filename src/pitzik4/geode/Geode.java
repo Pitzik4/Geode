dd3dd79c1577c39e3c9cc0ba9530c9e653ba469a
misson20000 <collin@collinswebsite.com>
2012-03-24 15:31:00 -0700

    Added some shutdown code

--- src/pitzik4/geode/Geode.java
+++ src/pitzik4/geode/Geode.java
@@ -2,4 +2,14 @@ package pitzik4.geode;
 
 public class Geode {
 
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
